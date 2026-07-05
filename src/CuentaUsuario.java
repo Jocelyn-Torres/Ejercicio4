2e8574cd511253a09f9ad2420e148fbdf93218a3
Jocelyn <tjocelyn0810@gmail.com>
2026-07-05 09:55:59 -0600

    Agregue atributos y constructores

--- src/CuentaUsuario.java
+++ src/CuentaUsuario.java
@@ -1,4 +1,39 @@
 public abstract class CuentaUsuario {
     private String correoElectronico;
     private int mesesActivo;
+    private PlanSuscripcion plan;
+
+    public CuentaUsuario(String correoElectronico, int mesesActivo, PlanSuscripcion plan) {
+        this.correoElectronico = correoElectronico;
+        this.mesesActivo = mesesActivo;
+        this.plan = plan;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public int getMesesActivo() {
+        return mesesActivo;
+    }
+
+    public void setMesesActivo(int mesesActivo) {
+        this.mesesActivo = mesesActivo;
+    }
+
+    public PlanSuscripcion getPlan() {
+        return plan;
+    }
+
+    public void setPlan(PlanSuscripcion plan) {
+        this.plan = plan;
+    }
+
+    public double obtenerTotalAPagar() {
+        return plan.calcularCosto(mesesActivo);
+    }
 }
