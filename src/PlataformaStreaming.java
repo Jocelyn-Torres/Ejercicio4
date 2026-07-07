92a415380a79620aa2906a83733cd0cf97e58270
Jocelyn <tjocelyn0810@gmail.com>
2026-07-06 19:42:42 -0600

    agregue lista dinamica y metodo de registrar

--- src/PlataformaStreaming.java
+++ src/PlataformaStreaming.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class PlataformaStreaming {
+
+    private ArrayList<CuentaUsuario> usuarios;
+
+    public PlataformaStreaming(ArrayList<CuentaUsuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public void registrarUsuarios(CuentaUsuario u){
+        usuarios.add(u);
+    }
+
+
 
 }
