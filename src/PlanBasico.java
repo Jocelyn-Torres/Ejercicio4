323593717fc489df00933f29f8ea6bab79b61b3b
Jocelyn <tjocelyn0810@gmail.com>
2026-07-07 09:19:59 -0600

    agregue las reglas de los planes

--- src/PlanBasico.java
+++ src/PlanBasico.java
@@ -1,8 +1,19 @@
-public class PlanBasico implements PlanSuscripcion{
+public class PlanBasico implements PlanSuscripcion {
+    private final double precio = 5.00;
+    private final int pantallas = 1;
+    private final String calidad = "SD";
+
 
     @Override
-    public double clacularCosto(int meses) {
-        return meses * 5;
+    public double calcularCosto(int meses) {
+        return meses * precio;
     }
 
+
+    public void mostrarInfo(){
+        System.out.println("---PLAN BASICO---");
+        System.out.println("Precio: $" + precio + "USD por mes");
+        System.out.println("Pantallas: " + pantallas);
+        System.out.println("Calidad: " + calidad);
+    }
 }
--- src/PlanEstandar.java
+++ src/PlanEstandar.java
@@ -1,6 +1,16 @@
 public class PlanEstandar implements PlanSuscripcion {
+    private final double precio = 9.00;
+    private final int pantallas = 2;
+    private final String calidad = "Full HD";
+
     @Override
     public double calcularCosto(int meses){
         return meses * 9;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Precio: $" + precio + "USD por mes");
+        System.out.println("Pantallas: " + pantallas );
+        System.out.println("Calidad: " + calidad);
     }
 }
